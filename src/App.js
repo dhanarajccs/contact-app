c116da1ae93a4e6e1a2c8ad45d6fd8a35b3c333c
dhanaraj ccs <dhanarajccs@gmail.com>
2022-02-03 21:44:22 +0530

    React JSX in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import React from 'react';    // imr
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <div>Hello World</div>
   );
 }
 
